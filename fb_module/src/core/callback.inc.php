96a859545adbd5386cc440d90967136433661271
matico <matico@10.0.0.107>
2016-03-21 13:44:10 +0700

    Mon Mar 21 13:44:02 ICT 2016

--- fb_module/src/core/callback.inc.php
+++ fb_module/src/core/callback.inc.php
@@ -1,22 +1,52 @@
 <?php
 require_once dirname ( __FILE__ ) . '/fbapi.php';
+require_once dirname ( __FILE__ ) . '/../db/FBDBProcess.php';
 $fb = fbapi_instance ();
 $helper = $fb->getRedirectLoginHelper ();
 try {
 	$accessToken = $helper->getAccessToken ();
 } catch ( Facebook\Exceptions\FacebookResponseException $e ) {
 	// When Graph returns an error
-	echo 'Graph returned an error: ' . $e->getMessage ();
-	exit ();
+	LoggerConfiguration::logError ( 'Graph returned an error: ' . $e->getMessage (), __FILE__, '', __LINE__ );
+	echo 'FBAPI_ERROR';
+	exit ( 0 );
 } catch ( Facebook\Exceptions\FacebookSDKException $e ) {
 	// When validation fails or other local issues
-	echo 'Facebook SDK returned an error: ' . $e->getMessage ();
-	exit ();
+	LoggerConfiguration::logError ( 'Facebook SDK returned an error: ' . $e->getMessage (), __FILE__, '', __LINE__ );
+	echo 'FBAPI_ERROR';
+	exit ( 0 );
 }
 if (isset ( $accessToken )) {
 	$accessToken = ( string ) $accessToken;
 	$_SESSION ['token'] = $accessToken;
+	$fp_core = new Fanpage ();
+	$fanpage_list = $fp_core->get_list ( $accessToken );
+	if ($fanpage_list) {
+		$db = new FBDBProcess ();
+		// group_id duoc luu vao session khi user login he thong
+		$group_id = $_SESSION ['group_id'];
+		$created_time = time ();
+		$added_pages = null;
+		foreach ( $fanpage_list as $page ) {
+			$page_id = $page ['id'];
+			$page_name = $page ['name'];
+			$token = $page ['accesstoken'];
+			$page_id = $page ['id'];
+			if ($db->storePages ( $group_id, $page_id, $page_name, $token, $created_time ) !== false) {
+				$added_pages [] = $page_id;
+			} else
+				break;
+		}
+		if ($added_pages)
+			echo 'SUCCESS';
+		else
+			echo 'PROCESS_FAIL';
+	} else {
+		LoggerConfiguration::logError ( 'Not found fanpage for user', __FILE__, '', __LINE__ );
+		echo 'NOTFOUND';
+	}
 } else {
-	echo 'Can not get access token';
+	LoggerConfiguration::logError ( 'Not found user accesstoken', __FILE__, '', __LINE__ );
+	echo 'ACCESSTOKEN_ERROR';
 	exit ( 0 );
 }
--- fb_module/src/db/DBProcess.php
+++ fb_module/src/db/DBProcess.php
@@ -2,26 +2,26 @@
 require_once dirname ( __FILE__ ) . '/../logger/LoggerConfiguration.php';
 require_once dirname ( __FILE__ ) . '/DBMysql.php';
 class DBProcess {
-	public $prefix = 'gom_';
+	protected $prefix = 'gom_';
 	public $error = null;
 	private $connection = null;
-	public function __construct() {
+	protected function __construct() {
 		try {
 			$this->connection = DBMysql::getInstance ();
 		} catch ( Exception $e ) {
 			$this->error = $e->getMessage ();
 		}
 	}
-	public function getConnection() {
+	protected function getConnection() {
 		return $this->connection->getConnection ();
 	}
-	public function real_escape_string($str) {
+	protected function real_escape_string($str) {
 		return mysqli_real_escape_string ( $this->getConnection (), $str );
 	}
-	public function free_result(&$result) {
+	protected function free_result(&$result) {
 		return mysqli_free_result ( $result );
 	}
-	public function query($query) {
+	protected function query($query) {
 		try {
 			return mysqli_query ( $this->connection->getConnection (), $query );
 		} catch ( Exception $e ) {
@@ -29,19 +29,19 @@ class DBProcess {
 			return false;
 		}
 	}
-	public function set_auto_commit($mode) {
+	protected function set_auto_commit($mode) {
 		return mysqli_autocommit ( $this->connection->getConnection (), $mode );
 	}
-	public function commit() {
+	protected function commit() {
 		return mysqli_commit ( $this->connection->getConnection () );
 	}
-	public function rollback() {
+	protected function rollback() {
 		return mysqli_rollback ( $this->connection->getConnection () );
 	}
-	public function insert_id() {
+	protected function insert_id() {
 		return mysqli_insert_id ( $this->connection->getConnection () );
 	}
-	public function get_error() {
+	protected function get_error() {
 		return mysqli_error ( $this->connection->getConnection () );
 	}
 }
