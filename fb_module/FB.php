0b957022a50262298797d90cc21c064c2782f6fc
matico <matico@192.168.4.112>
2016-03-21 22:49:39 +0700

    xxx

--- fb_module/FB.php
+++ fb_module/FB.php
@@ -19,6 +19,7 @@ class FB {
 			}
 			LoggerConfiguration::logInfo ( 'STEP 2: PROCESS POST' );
 			foreach ( $posts as $post ) {
+				$update_data = array ();
 				// xy lu tung post
 				LoggerConfiguration::logInfo ( "PostID: {$post['post_id']}, PageID: {$post['page_id']}, GroupID: {$post['group_id']}" );
 				$fanpage_token_key = $post ['token'];
@@ -48,7 +49,7 @@ class FB {
 							// 1. tao order
 							LoggerConfiguration::logInfo ( 'Create order' );
 							// 2. an comment (neu cau hinh cho phep)
-							if ($config ['disable_comment']) {
+							if ($post ['disable_comment']) {
 								LoggerConfiguration::logInfo ( 'Hide comment' );
 								if (! $fp->hide_comment ( $comment_id, $post_id, $page_id, $fanpage_token_key )) {
 									LoggerConfiguration::logError ( "Hide comment error: {$fp->error}", __CLASS__, __FUNCTION__, __LINE__ );
@@ -73,19 +74,47 @@ class FB {
 					if ($post ['nodata_number_day'] >= $config ['max_nodata_comment_day']) {
 						LoggerConfiguration::logInfo ( "nodata_number_day={$post ['nodata_number_day']} >= max_nodata_comment_day={$config ['max_nodata_comment_day']}" );
 						if ($current_day > $last_day) {
+							// thoi diem thuc hien cronjob la cua ngay hom sau (lan dau)
 							LoggerConfiguration::logInfo ( "current_day=$current_day > last_day=$last_day" );
 							LoggerConfiguration::logInfo ( 'Reset nodata_number_day' );
+							$update_data ['nodata_number_day'] = 0;
 							// reset so dem nodata_number_day & gian cach thoi gian lan xu ly sau
+							$next_level_fetch_comment = intval ( $post ['level_fetch_comment'] ) + 1;
+							$update_data ['level_fetch_comment'] = $next_level_fetch_comment;
+							if (isset ( $config ['level_fetch_comment'] [$next_level_fetch_comment] )) {
+								// dua vao level de dat thoi gian xu ly tiep theo
+								$next_time_fetch_comment = intval ( $post ['next_time_fetch_comment'] ) + $config ['level_fetch_comment'] [$next_level_fetch_comment];
+								$update_data ['next_time_fetch_comment'] = $next_time_fetch_comment;
+							} else {
+								// vuot qua so level cau hinh => bo qua post nay
+								$update_data ['status'] = 1;
+							}
+						} else {
+							// van thuc hien trong ngay hom do => update lai thoi diem lay comment (theo level hien tai)
 						}
+					} else {
+						// update lai thoi diem lay comment (theo level hien tai)
+					}
+					// update lai thoi diem lay comment (theo level hien tai)
+					if (isset ( $config ['level_fetch_comment'] [$post ['level_fetch_comment']] )) {
+						$update_data ['next_time_fetch_comment'] = intval ( $post ['next_time_fetch_comment'] ) + $config ['level_fetch_comment'] [$post ['level_fetch_comment']];
+					} else {
+						$update_data ['status'] = 1;
 					}
 				} else {
-					if ($post ['nodata_number_day'] > 0) {
-						LoggerConfiguration::logInfo ( 'Reset nodata_number_day' );
-						// reset so dem nodata_number_day
-					}
+					LoggerConfiguration::logInfo ( 'Found any comments for this post' );
+					// luu lai thoi diem thuc hien => lan sau chi lay comment tu thoi diem nay tro di thoi => tang toc he thong
+					$update_data ['last_time_fetch_comment'] = $current_time;
+					// reset so dem nodata_number_day
+					$update_data ['nodata_number_day'] = 0;
+					// dua level ve 0 de cho phep lay comment nhanh hon
+					$update_data ['level_fetch_comment'] = 0;
+					$update_data ['next_time_fetch_comment'] = intval ( $post ['next_time_fetch_comment'] ) + $config ['level_fetch_comment'] [0];
 				}
 				// cap nhat du lieu post
 				LoggerConfiguration::logInfo ( 'Update for this post' );
+				LoggerConfiguration::logInfo ( print_r ( $update_data, true ) );
+				$db->updatePost ( $post_id, $update_data );
 			}
 			if (count ( $posts ) < $config ['load_post_limit']) {
 				LoggerConfiguration::logInfo ( 'Over post data' );
@@ -104,7 +133,11 @@ class FB {
 		return array (
 				'load_post_limit' => LOAD_POST_LIMIT,
 				'fb_limit_comment_post' => FB_LIMIT_COMMENT_POST,
-				'max_nodata_comment_day' => 5 
-		);
+				'max_nodata_comment_day' => 5,
+				'level_fetch_comment' => array (
+						0 => 120, // cu 2 phut duoc phep lay 1 lan,
+						1 => 180 
+				) 
+		); // cu 3 phut duoc phep lay 1 lan
 	}
 }
