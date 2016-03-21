e1e3d60e7bc733db79de4ca2fb2eef09932967e6
matico <matico@10.0.0.107>
2016-03-21 15:06:04 +0700

    xxx

--- fb_module/src/core/callback.inc.php
+++ fb_module/src/core/callback.inc.php
@@ -19,24 +19,29 @@ try {
 }
 if (isset ( $accessToken )) {
 	$accessToken = ( string ) $accessToken;
+	LoggerConfiguration::init ( "User FB AccessToken=$accessToken" );
 	$_SESSION ['token'] = $accessToken;
+	$db = new FBDBProcess ();
 	$fp_core = new Fanpage ();
+	// luu user accesstoken
+	// group_id duoc luu vao session khi user login he thong
+	$group_id = empty ( $_SESSION ['group_id'] ) ? 1 : $_SESSION ['group_id'];
+	LoggerConfiguration::logInfo ( "Group ID=$group_id" );
+	$db->storeFBUserGroup ( $group_id, '', $accessToken );
 	$fanpage_list = $fp_core->get_list ( $accessToken );
 	if ($fanpage_list) {
-		$db = new FBDBProcess ();
-		// group_id duoc luu vao session khi user login he thong
-		$group_id = empty ( $_SESSION ['group_id'] ) ? 1 : $_SESSION ['group_id'];
+		LoggerConfiguration::logInfo ( 'Fanpage list:' . print_r ( $fanpage_list, true ) );
 		$created_time = time ();
 		$added_pages = null;
 		foreach ( $fanpage_list as $page ) {
 			$page_id = $page ['id'];
 			$page_name = $page ['name'];
-			$token = $page ['accesstoken'];
+			$token = $page ['access_token'];
 			$page_id = $page ['id'];
-			if ($db->storePages ( $group_id, $page_id, $page_name, $token, $created_time ) !== false) {
-				$added_pages [] = $page_id;
-			} else
+			if ($db->storePages ( $group_id, $page_id, $page_name, $token, $created_time ) === false) {
 				break;
+			} else
+				$added_pages [] = $page_id;
 		}
 		if ($added_pages)
 			echo 'SUCCESS';
--- fb_module/src/db/FBDBProcess.php
+++ fb_module/src/db/FBDBProcess.php
@@ -14,12 +14,29 @@ class FBDBProcess extends DBProcess {
 		$insert = "($group_id,'$page_id','$page_name','$token',$created_time,0,'$current_time','$current_time') ON DUPLICATE KEY UPDATE page_name='$page_name',token='$token,modified='$current_time'";
 		try {
 			$query = "INSERT INTO fb_pages(group_id,page_id,page_name,token,user_created,created,modified) VALUES $insert";
+			LoggerConfiguration::logInfo ( $query );
 			$this->query ( $query );
 			if ($this->error) {
 				LoggerConfiguration::logError ( $this->error, __CLASS__, __FUNCTION_, __LINE__ );
 				return false;
 			}
 			return $this->insert_id ();
+		} catch ( Exception $e ) {
+			LoggerConfiguration::logError ( $e->getMessage (), __CLASS__, __FUNCTION_, __LINE__ );
+			return false;
+		}
+	}
+	public function storeFBUserGroup($group_id, $fb_user_id, $token) {
+		$current_time = date ( 'd-m-Y H:i:s' );
+		try {
+			$query = "UPDATE groups SET fb_user_id='$fb_user_id',fb_user_token='$token',modified='$current_time' WHERE id=$group_id";
+			LoggerConfiguration::logInfo ( $query );
+			$this->query ( $query );
+			if ($this->error) {
+				LoggerConfiguration::logError ( $this->error, __CLASS__, __FUNCTION_, __LINE__ );
+				return false;
+			}
+			return true;
 		} catch ( Exception $e ) {
 			LoggerConfiguration::logError ( $e->getMessage (), __CLASS__, __FUNCTION_, __LINE__ );
 			return false;
