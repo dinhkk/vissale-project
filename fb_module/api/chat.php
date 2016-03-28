a32634ebff841ef36632697ead8f0abba309f2ff
matico <matico@10.0.0.103>
2016-03-28 11:13:27 +0700

    xxx

--- fb_module/api/chat.php
+++ fb_module/api/chat.php
@@ -5,17 +5,17 @@
 require_once dirname ( __FILE__ ) . '/../FB.php';
 $chat_group_id = intval ( $_GET ['chat_group_id'] );
 if (! $chat_group_id) {
-	echo 'ERROR';
+	echo 'ERROR1';
 	exit ( 0 );
 }
 $type = $_GET ['type'];
-if ($type !== 'comment' && $type !== 'inbox') {
-	echo 'ERROR';
+if (($type !== 'comment') && ($type !== 'inbox')) {
+	echo 'ERROR2';
 	exit ( 0 );
 }
 $message = trim ( $_GET ['message'] );
 if (empty ( $message )) {
-	echo 'ERROR';
+	echo 'ERROR3';
 	exit ( 0 );
 }
 $fb = new FB ();
@@ -23,5 +23,5 @@ if ($fb->chat ( $chat_group_id, $message, $type )) {
 	echo 'SUCCESS';
 	exit ( 0 );
 }
-echo 'ERROR';
+echo 'ERROR4';
 exit ( 0 );
