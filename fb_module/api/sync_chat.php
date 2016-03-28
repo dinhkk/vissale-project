24cbe7a94eb96296b6b93af8a230b4ea8014d90d
matico <matico@192.168.4.101>
2016-03-28 20:20:07 +0700

    xxx

--- fb_module/api/sync_chat.php
+++ fb_module/api/sync_chat.php
@@ -6,17 +6,17 @@ require_once dirname ( __FILE__ ) . '/../FB.php';
 $fb = new FB ();
 $group_chat_id = intval ( $_GET ['group_chat_id'] );
 if (! $group_chat_id) {
-	echo 'ERROR1';
+	echo 'ERROR';
 	exit ( 0 );
 }
 $type = $_GET ['type'];
 if (($type !== 'comment') && ($type !== 'inbox')) {
-	echo 'ERROR2';
+	echo 'ERROR';
 	exit ( 0 );
 }
 if ($fb->syncChat ( $group_chat_id, $type )) {
 	echo 'SUCCESS';
 	exit ( 0 );
 }
-echo 'ERROR3';
+echo 'ERROR';
 exit ( 0 );
