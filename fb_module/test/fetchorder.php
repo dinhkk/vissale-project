0020dde4f65de6137877a942d5b5191f4db7ae66
matico <matico@10.0.0.103>
2016-03-28 11:03:07 +0700

    xxx

--- fb_module/test/fetchorder.php
+++ fb_module/test/fetchorder.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Thuc hien dong bo noi dung conversation
+*/
+// test
 require_once dirname ( __FILE__ ) . '/../FB.php';
-$fb = new FB();
-$fb->fetchOrder(6, 'WORKER_1', '45.117.80.243');
+$fb = new FB ();
+if ($fb->syncChat ( 6, 'inbox' )) {
+	echo 'SUCCESS';
+	exit ( 0 );
+}
+echo 'ERROR';
+exit ( 0 );
