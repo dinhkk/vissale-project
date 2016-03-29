a262bd739e1345c9e9198f0700beb2524e4fe8ce
matico <matico@192.168.4.102>
2016-03-29 22:17:31 +0700

    xxx

--- fb_module/src/core/Fanpage.core.php
+++ fb_module/src/core/Fanpage.core.php
@@ -412,16 +412,13 @@ class Fanpage {
 		if (! $a) {
 			return false;
 		}
-		var_dump ( $a );
 		$url = curl_getinfo ( $ch, CURLINFO_EFFECTIVE_URL ); // This is what you need, it will return you the last effective URL
 		if (! $url) {
 			return false;
 		}
-		var_dump ( $url );
 		$parts = parse_url ( $url );
 		parse_str ( $parts ['query'], $query );
-		var_dump ( $query );
-		if (array_key_exists ( $query, 'id' )) {
+		if (is_array($query) && array_key_exists ( 'id', $query )) {
 			return $query ['id']; // page_id
 		}
 		return false;
