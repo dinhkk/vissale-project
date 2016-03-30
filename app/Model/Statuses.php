94f7af3865c5be86bcfe5adc42bda7bdc12af01c
matico <matico@192.168.100.132>
2016-03-30 16:00:47 +0700

    xxxx

--- app/Model/Statuses.php
+++ app/Model/Statuses.php
@@ -1,5 +1,5 @@
 <?php
-App::uses('AppModel', 'Model');
+App::uses ( 'AppModel', 'Model' );
 /**
  * orders Model
  *
@@ -15,61 +15,63 @@ App::uses('AppModel', 'Model');
  * @property Product $Product
  */
 class Statuses extends AppModel {
-
-/**
- * Validation rules
- *
- * @var array
- */
-	public $validate = array(
-		'code' => array(
-			'notBlank' => array(
-				'rule' => array('notBlank'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
+	public $useTable = 'statuses';
+	/**
+	 * Validation rules
+	 *
+	 * @var array
+	 */
+	public $validate = array (
+			'code' => array (
+					'notBlank' => array (
+							'rule' => array (
+									'notBlank' 
+							) 
+					)
+					// 'message' => 'Your custom message here',
+					// 'allowEmpty' => false,
+					// 'required' => false,
+					// 'last' => false, // Stop validation after this rule
+					// 'on' => 'create', // Limit validation to 'create' or 'update' operations
+					 
+			) 
 	);
-
+	
 	// The Associations below have been created with all possible keys, those that are not needed can be removed
-
-/**
- * belongsTo associations
- *
- * @var array
- */
-	public $belongsTo = array(
-		'Group' => array(
-			'className' => 'Group',
-			'foreignKey' => 'group_id',
-			'conditions' => '',
-			'fields' => '',
-			'order' => ''
-		)
+	
+	/**
+	 * belongsTo associations
+	 *
+	 * @var array
+	 */
+	public $belongsTo = array (
+			'Group' => array (
+					'className' => 'Group',
+					'foreignKey' => 'group_id',
+					'conditions' => '',
+					'fields' => '',
+					'order' => '' 
+			) 
 	);
-
-/**
- * hasAndBelongsToMany associations
- *
- * @var array
- */
-	public $hasAndBelongsToMany = array(
-		'Product' => array(
-			'className' => 'Product',
-			'joinTable' => 'orders_products',
-			'foreignKey' => 'orders_id',
-			'associationForeignKey' => 'product_id',
-			'unique' => 'keepExisting',
-			'conditions' => '',
-			'fields' => '',
-			'order' => '',
-			'limit' => '',
-			'offset' => '',
-			'finderQuery' => '',
-		)
+	
+	/**
+	 * hasAndBelongsToMany associations
+	 *
+	 * @var array
+	 */
+	public $hasAndBelongsToMany = array (
+			'Product' => array (
+					'className' => 'Product',
+					'joinTable' => 'orders_products',
+					'foreignKey' => 'orders_id',
+					'associationForeignKey' => 'product_id',
+					'unique' => 'keepExisting',
+					'conditions' => '',
+					'fields' => '',
+					'order' => '',
+					'limit' => '',
+					'offset' => '',
+					'finderQuery' => '' 
+			) 
 	);
-
 }
