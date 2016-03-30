417afd042ff494c4642ee61a1b567a28c58230fc
nhanhoavps <you@dinhkk.com>
2016-03-30 15:21:31 +0700

    aaa

--- app/Controller/OrdersController.php
+++ app/Controller/OrdersController.php
@@ -1,15 +1,24 @@
 <?php
-App::uses('AppController', 'Controller');
+App::uses ( 'AppController', 'Controller' );
 /**
  * Orders Controller
  */
 class OrdersController extends AppController {
-
-/**
- * Scaffold
- *
- * @var mixed
- */
+	
+	/**
+	 * Scaffold
+	 *
+	 * @var mixed
+	 */
 	public $scaffold;
-
+	public function index() {
+		$options = array ();
+		$options ['order'] = array (
+				'created' => 'DESC' 
+		);
+		$options ['conditions'] ['group_id'] = 1;
+		$this->Paginator->settings = $options;
+		$list_order = $this->Paginator->paginate ( 'Orders' );
+		$this->set ( 'orders', $list_order );
+	}
 }
