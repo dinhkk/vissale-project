b302bcdd4e5aba7d4e165c16d80589bf896f1408
matico <matico@192.168.10.13>
2016-04-06 17:02:12 +0700

    xxx

---
+++ app/Model/FBCronConfig.php
@@ -0,0 +1,36 @@
+<?php
+App::uses ( 'AppModel', 'Model' );
+/**
+ * orders Model
+ *
+ * @property Group $Group
+ * @property FbCustomer $FbCustomer
+ * @property FbPage $FbPage
+ * @property FbPost $FbPost
+ * @property FbComment $FbComment
+ * @property ShippingService $ShippingService
+ * @property Bundle $Bundle
+ * @property Status $Status
+ * @property Duplicate $Duplicate
+ * @property Product $Product
+ */
+class FBCronConfig extends AppModel {
+	public $useTable = 'fb_cron_config';
+	/**
+	 * Validation rules
+	 *
+	 * @var array
+	 */
+	public $validate = array (
+	);
+	
+	// The Associations below have been created with all possible keys, those that are not needed can be removed
+	
+	/**
+	 * belongsTo associations
+	 *
+	 * @var array
+	 */
+	public $belongsTo = array (
+	);
+}
