09664cdffdf833f4cf1c42343ef0d132b26a75e3
matico <matico@192.168.100.107>
2016-04-18 15:40:25 +0700

    xxx

---
+++ app/Model/Status.php
@@ -0,0 +1,61 @@
+<?php
+App::uses ( 'AppModel', 'Model' );
+/**
+ * orders Model
+ *
+ * @property Group $Group
+ * @property FbCustomer $FbCustomer
+ * @property FbPage $FbPage
+ * @property FbPost $FbPost
+ * @property FbComment $FbComment
+ * @property ShippingService $ShippingService
+ * @property Bundle $Bundle
+ * @property Status $Status
+ * @property Duplicate $Duplicate
+ * @property Product $Product
+ */
+class Status extends AppModel {
+	public $useTable = 'statuses';
+	public $actsAs = array('Search.Searchable');
+	
+	public $filterArgs = array(
+			'name' => array(
+					'type' => 'like',
+					'field' => 'name'
+			),
+	);
+	/**
+	 * Validation rules
+	 *
+	 * @var array
+	 */
+	public $validate = array (
+			'code' => array (
+					'notBlank' => array (
+							'rule' => array (
+									'notBlank' 
+							) 
+					)
+					// 'message' => 'Your custom message here',
+					// 'allowEmpty' => false,
+					// 'required' => false,
+					// 'last' => false, // Stop validation after this rule
+					// 'on' => 'create', // Limit validation to 'create' or 'update' operations
+					 
+			) 
+	);
+	
+	// The Associations below have been created with all possible keys, those that are not needed can be removed
+	
+	/**
+	 * belongsTo associations
+	 *
+	 * @var array
+	 */
+	public $belongsTo = array (
+// 			'Group' => array (
+// 					'className' => 'Group',
+// 					'foreignKey' => 'group_id'
+// 			) 
+	);
+}
