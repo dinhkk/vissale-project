c177ba082296e236e1e2f39351c0a3d746e38cc2
TIEN CONG MAC <TIEN CONG MAC>
2016-05-07 11:38:33 +0700

    xxx

--- fb_module/src/core/config.php
+++ fb_module/src/core/config.php
@@ -2,14 +2,15 @@
 /**
  * Cau hinh App facebook
  */
-define ( 'FB_APP_ID', '967811573302640' );
-define ( 'FB_APP_SECRET', '51b9c0cac5a277d1c86e356ad2a13b64' );
-define ( 'FB_API_VER', 'v2.5' );
-define ( 'FB_APP_DOMAIN', 'http://login.vingrowth.com/' );
+define('FB_APP_ID', '967811573302640');
+define('FB_APP_SECRET', '51b9c0cac5a277d1c86e356ad2a13b64');
+define('FB_API_VER', 'v2.5');
+define('FB_APP_DOMAIN', 'http://login.vingrowth.com/');
+define('CALLBACK_AFTER_SYNCPAGE', 'FBPage');
 
 /**
  * Trang thoi don hang mac dinh
  */
 
-define ( 'ORDER_STATUS_SUCCESS', 5 ); // don hang da thanh cong
-define ( 'ORDER_STATUS_CANCELED', 9 ); // don hang da bi huy
+define('ORDER_STATUS_SUCCESS', 5); // don hang da thanh cong
+define('ORDER_STATUS_CANCELED', 9); // don hang da bi huy
