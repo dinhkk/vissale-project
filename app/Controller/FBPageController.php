64ac8d6d57dcdbfa9c37ea2743e375c0236d2853
dinhkk <thedinh.trinh@gmail.com>
2016-06-20 23:08:47 +0700

    - FBPage -- synchronization

--- app/Controller/FBPageController.php
+++ app/Controller/FBPageController.php
@@ -44,7 +44,8 @@ class FBPageController extends AppController {
 		foreach ( $configs as $key => $value ) {
 			$this->set ( $key, $value );
 		}
-		$this->set ( 'fblogin_url', Configure::read ( 'sysconfig.FBPage.FB_LOGIN' ) . "?group_id={$group_id}" );
+		//$this->set ( 'fblogin_url', Configure::read ( 'sysconfig.FBPage.FB_LOGIN' ) . "?group_id={$group_id}" );
+		$this->set ( 'fblogin_url', Configure::read ( 'sysconfig.FBPage.FB_ACTIVE_PAGE' ) . "?group_id={$group_id}" );
 	}
 	public function updateConfig() {
 		$this->layout = 'ajax';
