3d8f8c441b17b3b01f6856bddb5c4e329e315add
dinhkk <thedinh.trinh@gmail.com>
2016-08-02 23:41:57 +0700

    change to vissale.com

--- fb_module/src/core/config.php
+++ fb_module/src/core/config.php
@@ -2,11 +2,13 @@
 /**
  * Cau hinh App facebook
  */
+define('FB_APP_CALLBACK_URL', 'https://vissale.com/fb_module/fb_callback.php');
+define('FB_APP_VERIFY_TOKEN', '0aaffee84f94dc316242d01bb7c94690');
 define('FB_APP_ID', '967811573302640');
 define('FB_APP_SECRET', '51b9c0cac5a277d1c86e356ad2a13b64');
 define('FB_API_VER', 'v2.5');
-define('FB_APP_DOMAIN', 'http://login.vingrowth.com/');
-define('CALLBACK_AFTER_SYNCPAGE', 'http://fbsale.vingrowth.com/FBPage');
+define('FB_APP_DOMAIN', 'http://login.vissale.com/');
+define('CALLBACK_AFTER_SYNCPAGE', 'https://vissale.com/FBPage');
 
 /**
  * Trang thoi don hang mac dinh
@@ -14,3 +16,4 @@ define('CALLBACK_AFTER_SYNCPAGE', 'http://fbsale.vingrowth.com/FBPage');
 
 define('ORDER_STATUS_SUCCESS', 5); // don hang da thanh cong
 define('ORDER_STATUS_CANCELED', 9); // don hang da bi huy
+define('ORDER_STATUS_DEFAULT', 10); // trang thai mac dinh khi tao don hang
