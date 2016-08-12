fc2715f805eb9e124c278841bb79215375f74109
dinhkk <thedinh.trinh@gmail.com>
2016-08-12 09:38:43 +0700

    commit more files

---
+++ app/Test/Case/Controller/GroupsControllerTest.php
@@ -0,0 +1,39 @@
+<?php
+App::uses('GroupsController', 'Controller');
+
+/**
+ * GroupsController Test Case
+ */
+class GroupsControllerTest extends ControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'app.group',
+		'app.billing_print',
+		'app.bundle',
+		'app.f_b_conversation_message',
+		'app.f_b_cron_config',
+		'app.fb_customer',
+		'app.f_b_page',
+		'app.fb_post_comment',
+		'app.fb_post',
+		'app.order',
+		'app.product',
+		'app.role',
+		'app.user',
+		'app.users_role',
+		'app.shipping_service',
+		'app.status',
+		'app.stock_book',
+		'app.stock_delivering',
+		'app.stock_receiving',
+		'app.stock',
+		'app.supplier',
+		'app.unit'
+	);
+
+}
---
+++ app/webroot/fb_module/test/check_telco.php
@@ -0,0 +1,3 @@
+<?php
+require_once dirname(__FILE__) . '/../FB.php';
+var_dump((new FB())->_getTelcoByPhone('84966496489'));
