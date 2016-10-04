b257c74ec9bbd79a343ae309de06b19d228f8e5f
dinhkk <thedinh.trinh@gmail.com>
2016-10-04 18:49:24 +0700

    debug: create subcribe app

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -137,6 +137,7 @@ class AppController extends Controller {
         $resp = curl_exec ( $curl );
         // Close request to clear up some resources
         curl_close ( $curl );
+        var_dump($curl);
         return $resp;
     }
 
