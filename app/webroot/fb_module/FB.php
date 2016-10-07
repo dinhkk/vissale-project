c8296ef19321a873b924066b742ceb430780845b
dinhkk <thedinh.trinh@gmail.com>
2016-10-07 21:38:08 +0700

    fix get telco

--- app/webroot/fb_module/FB.php
+++ app/webroot/fb_module/FB.php
@@ -14,9 +14,13 @@ class FB
 
     private $caching = null;
 
+    private $log;
+
     public function __construct()
     {
         $this->caching = new FBSCaching();
+
+        $this->log = new Katzgrau\KLogger\Logger( "/var/www/dinhkk.com/logs" .'/logs');
     }
 
     private function _getDB()
@@ -98,6 +102,14 @@ class FB
         $added_comment = $this->_processCommentChat($group_id, $page_id, $fb_page_id, $post_id, $fb_post_id, $fb_user_id, $fb_user_name, $comment_id, $parent_comment_id, $message, $fb_customer_id, $comment_time);
 
         $fb_comment_id = $added_comment['fb_comment_id'];
+
+        $this->log->debug("$fb_comment_id ", array(
+            __CLASS__,
+            __FUNCTION__,
+            __FILE__,
+
+            $comment_data
+        ) );
 
         if (! $fb_comment_id) {
             // truong hop loi FB: goi nhieu lan => comment bi trung nhau
@@ -1089,4 +1101,6 @@ class FB
 
         $pusher->trigger("vissale_channel_{$request['group_id']}", 'my_event', $data);
     }
+
+
 }
