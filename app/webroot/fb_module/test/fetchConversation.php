53e4f11e50bdfe0ddd7126c2e2e385fe47eb8817
dinhkk <thedinh.trinh@gmail.com>
2016-10-11 17:57:34 +0700

    test save conversation

--- app/webroot/fb_module/test/fetchConversation.php
+++ app/webroot/fb_module/test/fetchConversation.php
@@ -1,4 +1,12 @@
 <?php
-require_once dirname ( __FILE__ ) . '/../FB.php';
+/*require_once dirname ( __FILE__ ) . '/../FB.php';
 $fb = new FB ();
-$fb->fetchConversation ( 8, 'WORKER_1', '45.117.80.243' );
+$fb->fetchConversation ( 8, 'WORKER_1', '45.117.80.243' );*/
+
+require_once dirname(__FILE__) . '/../src/core/config.php';
+require_once dirname(__FILE__) . '/../FB.php';
+require_once dirname(__FILE__) . '/../src/db/FBDBProcess.php';
+
+$db = new FBDBProcess();
+
+$db->loadConversation(null,null, "1109781745757296_1110150235720447");
