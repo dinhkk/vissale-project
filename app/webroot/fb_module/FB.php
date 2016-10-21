f23e985226d28a274872ad087266bca0aad7ae92
dinhkk <thedinh.trinh@gmail.com>
2016-10-21 16:16:23 +0700

    test chat inbox

--- app/webroot/fb_module/FB.php
+++ app/webroot/fb_module/FB.php
@@ -470,7 +470,8 @@ class FB
         LoggerConfiguration::logInfo('GET MESSAGE FROM CONVERSATION');
         $messages = $this->_loadFBAPI()->get_conversation_messages($thread_id, $page_id, $fanpage_token_key, null, $time, 1);
 
-        $this->log->debug("test chat inbox . message : {$messages}", array(
+        $this->log->debug("test chat inbox", array(
+            'messages' => $messages,
             '$page_id' => $page_id,
             '$data' => $data,
             '$time' => $time,
