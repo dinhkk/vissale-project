5766076833f6480b3d45922787d48371d770d41e
dinhkk <thedinh.trinh@gmail.com>
2016-11-06 17:35:43 +0700

    add namespace Service \ Group

--- app/webroot/fb_module/debug_callback.php
+++ app/webroot/fb_module/debug_callback.php
@@ -1,26 +1,6 @@
 <?php
-require_once("src/facebook_api/src/Facebook/autoload.php");
-$config = require_once("src/core/config.php");
+require_once("vendor/autoload.php");
 
-$fb = new Facebook\Facebook([
-    'app_id' => '1711414672510524',
-    'app_secret' => '3335bd7393039773ab4981490afc121f',
-    'default_graph_version' => 'v2.8',
-]);
+use Services\Group;
 
-$fb->setDefaultAccessToken('EAAYUhceeTjwBAKqfkyZAe5d5pnJblcE4vr3bnVzws8pbHvwATVkZBMlHJZBjaZAi8j1ZBeS5MSNZCFptpdQ2G7cQZCz6ZC9KqPJAqah1ERRB9ebWU2SOopBDs7uOIootAeaP635P5suPdjB29BoAZCz6FcNnkkWEGic9SdPPFJoxucgZDZD');
-
-
-try {
-    $response = $fb->get('/1162464830508422_1164707273617511');
-    $content = $response->getDecodedBody();
-    var_dump($content);
-} catch (Facebook\Exceptions\FacebookResponseException $e) {
-    // When Graph returns an error
-    echo 'Graph returned an error: ' . $e->getMessage();
-    exit;
-} catch (Facebook\Exceptions\FacebookSDKException $e) {
-    // When validation fails or other local issues
-    echo 'Facebook SDK returned an error: ' . $e->getMessage();
-    exit;
-}
+$group = new Group();
---
+++ app/webroot/fb_module/services/Group.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dinhkk
+ * Date: 11/5/16
+ * Time: 8:38 AM
+ */
+
+namespace Services;
+
+
+class Group
+{
+    public function __construct()
+    {
+        die('group service');
+    }
+}
