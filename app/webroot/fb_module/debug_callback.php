8cc5ff70bc51fd49a33cd4985f9c2239a14b0a84
dinhkk <thedinh.trinh@gmail.com>
2016-11-11 09:54:04 +0700

    debug call back

--- app/webroot/fb_module/debug_callback.php
+++ app/webroot/fb_module/debug_callback.php
@@ -49,7 +49,7 @@ if (preg_match('[time|current time|now]', strtolower($message))) {
         $message_to_reply = $result;
     }
 } else {
-    $message_to_reply = 'Huh! what do you mean?';
+    $message_to_reply = 'Huh! what do you mean? test';
 }
 
 //API Url
