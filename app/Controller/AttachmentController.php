85ace22ddb6d8aed9be6224a62a2e4a57a13d964
dinhkk <thedinh.trinh@gmail.com>
2016-11-12 18:00:46 +0700

    finish upload file

---
+++ app/Controller/AttachmentController.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dinhkk
+ * Date: 11/12/16
+ * Time: 4:32 PM
+ */
+
+
+App::uses('AppController', 'Controller');
+App::uses('Folder', 'Utility');
+App::uses('File', 'Utility');
+
+
+class AttachmentController extends AppController
+{
+
+    public function uploadFile()
+    {
+        $message = array(
+            'error' => 1,
+            'message' => 'Lỗi upload file !',
+            'data' => null
+        );
+
+        try {
+            $fileData = $_FILES;
+            $conversionId = $this->request->data['conversation_id'];
+            $folder = new Folder(WWW_ROOT);
+            $relativePath = "files/{$conversionId}";
+            $folder->create($relativePath);
+            $folder->cd($relativePath);
+
+            $file = new File($fileData['file_message']['tmp_name'], true, 0644);
+            $fileCopyPath = $folder->pwd() . "/" . uniqid('vissale_') . $fileData['file_message']['name'];
+            $file->copy($fileCopyPath, true);
+            $copiedFile = new File($fileCopyPath);
+
+            if ($copiedFile->readable()) {
+                $message['error'] = 0;
+                $message['message'] = 'Upload file thành công!';
+                $message['data'] = "https://" . $_SERVER['SERVER_NAME'] . "/{$relativePath}/{$fileData['file_message']['name']}";
+
+                echo json_encode($message);
+                die;
+            }
+
+        } catch (Exception $e) {
+            echo json_encode($message);
+            die;
+        }
+
+        die('error');
+    }
+
+}
