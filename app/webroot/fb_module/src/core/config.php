13c3b7d73dc3081a7db53234b5ad2c617344215c
dinhkk <thedinh.trinh@gmail.com>
2016-11-17 15:35:09 +0700

    fb_module: create publish function to Faye server

--- app/webroot/fb_module/src/core/config.php
+++ app/webroot/fb_module/src/core/config.php
@@ -111,6 +111,37 @@ ActiveRecord\Config::initialize(function ($cfg) use($path_orm) {
 });
 
 
+/**
+ * define pushing notification
+ * $data = array('data' => uniqid());
+ * $ext = array('ext' => 'sdssdf');
+ * $channel = "/foo";
+ * $url = "http://vissale.dev:8000/faye";
+ */
+if (!function_exists("postJSONFaye")) {
+    function postJSONFaye($server, $channel, Array $data = [], Array $ext = [])
+    {
+        $body = json_encode(array(
+            'channel' => $channel,
+            'data' => $data,
+            'ext' => $ext,
+        ));
+
+        $curl = curl_init($server);
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $body);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($body),
+        ));
+
+        $result = curl_exec($curl);
+        curl_close($curl);
+
+        return $result;
+    }
+}
 /*function createLog($message)
 {
     $current_time = date ( 'Y-m-d H:i:s' );
