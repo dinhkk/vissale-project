664c0f30c0dd9ec512b8e92a9dc9119906d2fd6a
dinhkk <thedinh.trinh@gmail.com>
2016-11-29 18:49:07 +0700

    debug current conversation

--- app/webroot/js/chat-app.js
+++ app/webroot/js/chat-app.js
@@ -536,7 +536,8 @@ function ObjectMessage(data) {
 				scrollToPositionChatHistory(__calculateHeightScrollTo());
 			}, 1000);
 			
-			console.info('current conversation', $scope.currentConversation);
+			console.info(conversation);
+			console.info($scope.currentConversation);
 		};
         
 		$scope.changePage = function () {
