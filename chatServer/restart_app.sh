6221705f88da42b09a7bff486d7d6322cd07d303
dinhkk <thedinh.trinh@gmail.com>
2016-11-29 19:33:33 +0700

    setup ENV development

--- chatServer/restart_app.sh
+++ chatServer/restart_app.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+
 pm2 stop app
-pm2 start app.js -x -- --prod --log-date-format "YYYY-MM-DD HH:mm Z"
+#pm2 start app.js -x -- --prod --log-date-format "YYYY-MM-DD HH:mm Z"
+pm2 start app.js --log-date-format "YYYY-MM-DD HH:mm Z"
 pm2 logs
