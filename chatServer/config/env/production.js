49051cd64b3ccb074b9fd344180c05158f045aab
dinhkk <thedinh.trinh@gmail.com>
2016-11-29 17:55:25 +0700

    config development

--- chatServer/config/env/production.js
+++ chatServer/config/env/production.js
@@ -17,9 +17,12 @@ module.exports = {
      * environment (see config/connections.js and config/models.js )           *
      ***************************************************************************/
 
-    // models: {
-    //   connection: 'someMysqlServer'
-    // },
+    models: {
+      connection: 'productionMysqlServer',
+      migrate: 'safe',
+      autoUpdatedAt: false,
+      autoCreatedAt: false
+    }
 
     /***************************************************************************
      * Set the port in the production environment to 80                        *
