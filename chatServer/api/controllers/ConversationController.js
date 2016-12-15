1addd6c0004f4168d6863d33c17d74dc50db2a3a
dinhkk <thedinh.trinh@gmail.com>
2016-12-15 09:49:26 +0700

    debug nodejs

--- chatServer/api/controllers/ConversationController.js
+++ chatServer/api/controllers/ConversationController.js
@@ -174,7 +174,8 @@ module.exports = {
       if (error) {
         return res.json(content);
       }
-      console.log('Send message successfully! Server responded with:', body.data, body);
+      console.log('Send message successfully! Server responded with:', body.data.success, body);
+      console.log('Body data is success:', body.data.success);
   
       if (body.success == true) {
         content.success = true;
