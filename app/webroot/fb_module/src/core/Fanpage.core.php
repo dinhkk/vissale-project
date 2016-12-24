33d1df0c4a06f3b324ec4cfbe086cca0bc4799f4
dinhkk <thedinh.trinh@gmail.com>
2016-12-24 21:54:31 +0700

    test attachment comment

--- app/webroot/fb_module/src/core/Fanpage.core.php
+++ app/webroot/fb_module/src/core/Fanpage.core.php
@@ -295,7 +295,7 @@ class Fanpage {
 
 			$res = $this->facebook_api->post ( $end_point, array (
 					'message' =>  "$message @$fb_user_name",
-                    'attachment_url' => $attachment_url
+                    'attachment_url' => "$attachment_url"
 			), $fanpage_token_key, null, $this->fb_api_ver );
 
 			LoggerConfiguration::logInfo ( "Reply for: $fb_user_id" );
