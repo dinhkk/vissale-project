81b756631155b59a3d94fc7f0e8512f717ca0ac9
dinhkk <thedinh.trinh@gmail.com>
2017-01-18 21:12:26 +0700

    debug only 2 pages — is_read inbox

--- app/webroot/fb_module/services/InboxObject.php
+++ app/webroot/fb_module/services/InboxObject.php
@@ -366,7 +366,7 @@ class InboxObject extends AppService
         $this->setGroupId( $inboxConversation['group_id'] );
         $this->setFbPageId( $inboxConversation['fb_page_id'] );
         $this->setHasOrder( $this->_includedPhone( $this->getMessage() ) );
-        $is_page = ( $this->getFbPageId() == $this->getFbUserId() ) ? true : false;
+        $is_page = ( $this->getFbPageId() == $this->getFbUserId() ) ? 1 : 0;
         $this->setIsPage( $is_page );
         $this->setIsRead( !$is_page );
         $this->setUsername( $inboxConversation['fb_user_name'] );
